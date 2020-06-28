5784cccb7681df5d5ec820a0c49574ae2cf5c775
huangGuanShanYao <huangguanshanyao@WuHaoJie-MacBook-Pro.local>
2020-06-28 16:49:37 +0800

    新增底部

--- src/containers/components/bottom/index.component.jsx
+++ src/containers/components/bottom/index.component.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import './index.less';
-
+import { HeartFilled, AlipayCircleOutlined, TaobaoOutlined } from '@ant-design/icons';
 class RootComponent extends React.Component {
-   
+
     render() {
         return (
             <div className='indexBottom' >
-                <div className='wrapper'></div>
-                
+                <div className='wrapper'>
+                    <div>
+                        <AlipayCircleOutlined style={{ fontSize: '25px' }} />
+                        <TaobaoOutlined  style={{ fontSize: '25px',marginLeft:'15px' }}/>
+                    </div>
+                    <div>
+                        <span>@2020 Casual的个人博客</span> <HeartFilled className='HeartFilled' style={{ color: 'red' }} />
+                    </div>
+                </div>
+
             </div>
         );
     }
